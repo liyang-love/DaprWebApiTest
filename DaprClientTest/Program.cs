c88fe6cac06d4a9125d28428e6cb6b4c046a605b
liyang <li13526285513@163.com>
2021-12-06 17:48:13 +0800

    添加项目文件。

---
+++ DaprClientTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using Dapr.Client;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace DaprClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var daprClient = new DaprClientBuilder().Build();
+
+            var confirmation = daprClient.InvokeMethodAsync<List<WeatherForecast>>(HttpMethod.Get, "DaprWebApiTestApp", "api/WeatherForecast/");
+
+            Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(confirmation.Result));
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
